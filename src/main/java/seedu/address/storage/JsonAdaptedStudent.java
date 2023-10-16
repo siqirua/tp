775983810ff767da17f03d014292064ec6ccc836
Vedant Sinha <vedantsinha2003@gmail.com>
2023-10-16 18:07:12 +0800

    Create new JsonAdaptedStudent class

---
+++ src/main/java/seedu/address/storage/JsonAdaptedStudent.java
@@ -0,0 +1,21 @@
+package seedu.address.storage;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.student.Student;
+import seedu.address.model.student.StudentEmail;
+import seedu.address.model.student.StudentId;
+import seedu.address.model.student.StudentName;
+import seedu.address.model.student.TutorialGroup;
+
+
+public class JsonAdaptedStudent {
+}
