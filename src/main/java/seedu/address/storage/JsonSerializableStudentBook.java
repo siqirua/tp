52672520d4b84d5b7df8ea17d031cc7dc47338ea
Vedant Sinha <vedantsinha2003@gmail.com>
2023-10-16 19:11:55 +0800

    Create class JsonSerializableStudentBook

---
+++ src/main/java/seedu/address/storage/JsonSerializableStudentBook.java
@@ -0,0 +1,59 @@
+package seedu.address.storage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.student.model.StudentBook;
+import seedu.address.model.student.model.ReadOnlyStudentBook;
+import seedu.address.model.student.Student;
+
+/**
+ * An Immutable AddressBook that is serializable to JSON format.
+ */
+@JsonRootName(value = "studentbook")
+public class JsonSerializableStudentBook {
+
+    public static final String MESSAGE_DUPLICATE_PERSON = "Persons list contains duplicate person(s).";
+
+    private final List<JsonAdaptedStudent> students = new ArrayList<>();
+
+    /**
+     * Constructs a {@code JsonSerializableStudentBook} with the given persons.
+     */
+    @JsonCreator
+    public JsonSerializableStudentBook(@JsonProperty("students") List<JsonAdaptedStudent> students) {
+        this.students.addAll(students);
+    }
+
+    /**
+     * Converts a given {@code ReadOnlyStudentBook} into this class for Jackson use.
+     *
+     * @param source future changes to this will not affect the created {@code JsonSerializableStudentBook}.
+     */
+    public JsonSerializableStudentBook(ReadOnlyStudentBook source) {
+        students.addAll(source.getStudentList().stream().map(JsonAdaptedStudent::new).collect(Collectors.toList()));
+    }
+
+    /**
+     * Converts this address book into the model's {@code AddressBook} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated.
+     */
+    public StudentBook toModelType() throws IllegalValueException {
+        StudentBook studentBook = new StudentBook();
+        for (JsonAdaptedStudent jsonAdaptedStudent : students) {
+            Student student = jsonAdaptedStudent.toModelType();
+            if (studentBook.hasStudent(student)) {
+                throw new IllegalValueException(MESSAGE_DUPLICATE_PERSON);
+            }
+            studentBook.addStudent(student);
+        }
+        return studentBook;
+    }
+}
