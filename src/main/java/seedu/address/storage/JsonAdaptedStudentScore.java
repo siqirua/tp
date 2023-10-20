607866d939498cabd5c8b5cc89352e36361d7fa7
Vedant Sinha <vedantsinha2003@gmail.com>
2023-10-20 20:17:31 +0800

    Fix some bug in JsonAdaptedStudentScore

--- src/main/java/seedu/address/storage/JsonAdaptedStudentScore.java
+++ src/main/java/seedu/address/storage/JsonAdaptedStudentScore.java
@@ -9,7 +9,6 @@ import seedu.address.model.gradedcomponent.GradedComponent;
 import seedu.address.model.gradedcomponent.MaxMarks;
 import seedu.address.model.gradedcomponent.Weightage;
 import seedu.address.model.student.StudentId;
-import seedu.address.model.student.StudentName;
 import seedu.address.model.studentscore.StudentScore;
 
 
@@ -22,7 +21,6 @@ public class JsonAdaptedStudentScore {
     private static final String DEFAULT_WEIGHTAGE = "100";
 
     private final String studentId;
-    private final String studentName;
     private final String gcName;
     private final String gcMaxMarks;
     private final String gcWeightage;
@@ -34,14 +32,12 @@ public class JsonAdaptedStudentScore {
      */
     @JsonCreator
     public JsonAdaptedStudentScore(@JsonProperty("studentId") String studentId,
-                                   @JsonProperty("studentName") String studentName,
                                    @JsonProperty("gradedComponentName") String gcName,
                                    @JsonProperty("gradedComponentMaximumMarks") String gcMax,
                                    @JsonProperty("gradedComponentWeightage") String gcWeightage,
                                    @JsonProperty("score") String score,
                                    @JsonProperty("comment") String comment) {
         this.studentId = studentId;
-        this.studentName = studentName;
         this.gcName = gcName;
         this.gcMaxMarks = gcMax;
         this.gcWeightage = gcWeightage;
@@ -54,7 +50,6 @@ public class JsonAdaptedStudentScore {
      */
     public JsonAdaptedStudentScore(StudentScore source) {
         studentId = source.getStudentId().sid;
-        studentName = source.getName().fullName;
         gcName = source.getGcName().gcName;
         gcMaxMarks = DEFAULT_MAX;
         gcWeightage = DEFAULT_WEIGHTAGE;
@@ -76,15 +71,6 @@ public class JsonAdaptedStudentScore {
             throw new IllegalValueException(StudentId.MESSAGE_CONSTRAINTS);
         }
         final StudentId modelStudentId = new StudentId(studentId);
-
-        if (studentName == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
-                StudentName.class.getSimpleName()));
-        }
-        if (!StudentName.isValidName(studentName)) {
-            throw new IllegalValueException(StudentName.MESSAGE_CONSTRAINTS);
-        }
-        final StudentName modelStudentName = new StudentName(studentName);
 
         final GcName newGcName = new GcName(gcName);
         final GradedComponent gradedComponent = new GradedComponent(newGcName,
