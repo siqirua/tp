89dc338ec211a4fe448009476223231b9c53e850
siqirua <lsq6918086@gmail.com>
2023-11-05 20:30:21 +0800

    Implement a Util class for easy testing and two sample test cases

---
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -0,0 +1,76 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.Model;
+import seedu.address.logic.commands.exceptions.CommandException;
+public class CommandTestUtil {
+    /**
+     * Executes the given {@code command} under positive test cases, checks if <br>
+     * - the returned {@link CommandResult} matches {@code expectedCommandResult} <br>
+     * - the {@code actualModel} after execution matches {@code expectedModel}
+     *
+     * @param command Command to be executed.
+     * @param actualModel Actual model before the execution.
+     * @param expectedResultString Expected string representing the expected command result.
+     * @param expectedModel Expected model after the execution.
+     */
+    public static void assertCommandSuccess(Command command, Model actualModel, String expectedResultString,
+                                            Model expectedModel) {
+
+        CommandResult expectedResult;
+        try {
+            expectedResult = new CommandResult(expectedResultString);
+        } catch (Exception e) {
+            throw new AssertionError("Command Result not successfully created.", e);
+        }
+        assertCommandSuccess(command, actualModel, expectedResult, expectedModel);
+    }
+
+    /**
+     * Executes the given {@code command} under positive test cases, checks if <br>
+     * - the returned {@link CommandResult} matches {@code expectedCommandResult} <br>
+     * - the {@code actualModel} after execution matches {@code expectedModel}
+     * @param command Command to be executed.
+     * @param actualModel Actual model before the execution.
+     * @param expectedResult Expected command result.
+     * @param expectedModel Expected model after the execution.
+     */
+    public static void assertCommandSuccess(Command command, Model actualModel, CommandResult expectedResult,
+                                            Model expectedModel) {
+        try {
+            CommandResult result = command.execute(actualModel);
+            assertEquals(expectedResult, result);
+            assertEquals(expectedModel, actualModel);
+        } catch (CommandException ce) {
+            throw new AssertionError("Execution of command in positive test cases should not fail.", ce);
+        }
+    }
+
+    /**
+     * Executes the given {@code command} under negative test cases, checks if <br>
+     * - the thrown {@link Exception} matches {@code expectedException} <br>
+     * - the {@code actualModel} after execution matches {@code expectedModel}
+     * @param command Command to be executed.
+     * @param actualModel Actual model before the execution.
+     * @param expectedException Expected exception to be thrown.
+     * @param expectedModel Expected model after the execution, usually the same as the actual model before execution.
+     */
+    public static void assertCommandSuccess(Command command, Model actualModel, CommandException expectedException,
+                                            Model expectedModel) {
+        try {
+            command.execute(actualModel);
+            throw new AssertionError("Execution of command in negative test cases should not succeed.");
+        } catch (CommandException ce) {
+            assertEquals(expectedException, ce);
+        } finally {
+            assertEquals(actualModel, expectedModel);
+        }
+    }
+}
