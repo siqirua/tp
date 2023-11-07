4c9804cf60e86b02f5bf67f569a13f10cf5f931e
siqirua <lsq6918086@gmail.com>
2023-11-07 19:34:34 +0800

    Fix some checkstyle issues

--- src/test/java/seedu/address/logic/commands/StatsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/StatsCommandTest.java
@@ -2,9 +2,9 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
