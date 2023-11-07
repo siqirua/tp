fe40c00522fc7e2d4355884a300fc24fcb36b918
marioalvaro <marioalvaro@Marios-MacBook-Pro.local>
2023-11-07 22:56:57 +0800

    Add more testing skeleton for EditStudentScoreCommandTest

--- src/test/java/seedu/address/logic/commands/EditStudentScoreCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditStudentScoreCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.model.studentscore.StudentScore;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,7 +9,7 @@ class EditStudentScoreCommandTest {
 
     @Test
     public void execute_notParsableIndex_failure() {
-
+        StudentScore studentScore;
     }
 
     @Test
@@ -44,11 +45,39 @@ class EditStudentScoreCommandTest {
     }
 
     @Test
+    public void execute_scoreNotParsable_failure() {
+    }
+
+    @Test
     public void execute_validEditComment_success() {
     }
 
     @Test
-    public void execute_editCommentWithNoValue_failure() {
+    public void execute_editCommentWithNoValue_success() {
+    }
+
+    @Test
+    public void execute_validEditTag_success() {
+    }
+
+    @Test
+    public void execute_invalidTagName_failure() { //invalid regex
+    }
+
+    @Test
+    public void execute_editTagWithNoValue_failure() {
+    }
+
+    @Test
+    public void execute_validMultipleTags_success() {
+    }
+
+    @Test
+    public void execute_duplicateTags_failure() { //not handled yet (error message)
+    }
+
+    @Test
+    public void execute_multipleTagsWithOneEmptyValue_failure() {
     }
 
     // More test on edit component dll
@@ -62,7 +91,7 @@ class EditStudentScoreCommandTest {
     }
 
     @Test
-    public void execute_() {
+    public void execute() {
     }
 
     @Test
