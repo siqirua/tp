bbe6fb054a7dada102ec5d2ccb7893aef4561bb8
marioalvaro <marioalvaro@Marios-MacBook-Pro.local>
2023-11-07 20:12:08 +0800

    Add testing skeleton for EditStudentScoreCommandTest

---
+++ src/test/java/seedu/address/logic/commands/EditStudentScoreCommandTest.java
@@ -0,0 +1,79 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EditStudentScoreCommandTest {
+
+    @Test
+    public void execute_notParsableIndex_failure() {
+
+    }
+
+    @Test
+    public void execute_zeroOrLessIndex_failure() {
+    }
+
+    @Test
+    public void execute_validIndex_success() {
+    }
+
+    @Test
+    public void execute_invalidPrefixToEdit_failure() {
+    }
+
+    @Test
+    public void execute_indexMoreThanListSize_failure() {
+    }
+
+    @Test
+    public void execute_validEditScore_success() {
+    }
+
+    @Test
+    public void execute_scoreAboveMaxMark_failure() { //?????
+    }
+
+    @Test
+    public void execute_negativeScore_failure() {
+    }
+
+    @Test
+    public void execute_editScoreWithNoValue_failure() {
+    }
+
+    @Test
+    public void execute_validEditComment_success() {
+    }
+
+    @Test
+    public void execute_editCommentWithNoValue_failure() {
+    }
+
+    // More test on edit component dll
+
+    @Test
+    public void execute_validMultipleEditComponents_success() {
+    }
+
+    @Test
+    public void execute_multipleEditComponentsWithOneInvalidInput_failure() { //??????
+    }
+
+    @Test
+    public void execute_() {
+    }
+
+    @Test
+    public void execute_() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
