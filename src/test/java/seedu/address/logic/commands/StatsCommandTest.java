439da30e43fe7d76b48c4baa06e1579f3264deb6
siqirua <lsq6918086@gmail.com>
2023-11-07 19:27:07 +0800

    Fix some checkstyle issues

--- src/test/java/seedu/address/logic/commands/StatsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/StatsCommandTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -11,15 +12,14 @@ import seedu.address.testutil.TestGcDataUtil;
 import seedu.address.testutil.TestStudentDataUtil;
 import seedu.address.testutil.TestStudentScoreDataUtil;
 
-import java.util.ArrayList;
 
 public class StatsCommandTest {
     private Model model = new ModelManager(TestStudentDataUtil.getTestStudentBook("score"),
             TestStudentScoreDataUtil.getSampleStudentScoreBook("create"),
             TestGcDataUtil.getTestGcBook("delete"), new UserPrefs());
-    private String expectedOutput = "Here are the statistics:\n" +
-            "MAX = 50.00, MIN = 0.00, MEAN = 25.00, STANDARD DEVIATION = 17.08\n" +
-            "UPPER QUARTILE = 42.50, LOWER QUARTILE = 7.50, SKEWNESS= 0.00\n";
+    private String expectedOutput = "Here are the statistics:\n"
+            + "MAX = 50.00, MIN = 0.00, MEAN = 25.00, STANDARD DEVIATION = 17.08\n"
+            + "UPPER QUARTILE = 42.50, LOWER QUARTILE = 7.50, SKEWNESS= 0.00\n";
 
     @Test
     public void execute_stat_success() {
