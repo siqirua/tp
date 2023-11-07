b0ad79722bc7ae134592d4071896b92e20eb090b
siqirua <lsq6918086@gmail.com>
2023-11-08 01:21:40 +0800

    Implement one more test case for StatsCommandTest

--- src/test/java/seedu/address/logic/commands/StatsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/StatsCommandTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -35,5 +36,22 @@ public class StatsCommandTest {
     public void execute_tutStat_success() {
         assertCommandSuccess(new StatsCommand(new ArrayList<>(), new TutorialGroup("T03")), model,
                 expectedOutputOfTut3, model);
+    }
+
+    //ADD ONE TEST TO CHECK SPECIFIC GRADED COMPONENT
+    @Test
+    public void execute_specificStat_success() {
+        String expectedOutput1 = "Here are the statistics:\n"
+                + "MAX = 100.00, MIN = 0.00, MEAN = 50.00, STANDARD DEVIATION = 34.16\n";
+        String expectedOutput2 = "Here are the statistics:\n"
+                + "UPPER QUARTILE = 85.00, LOWER QUARTILE = 15.00, SKEWNESS = 0.00\n";
+        ArrayList<String> stats1 = new ArrayList<>(List.of(new String[] {
+                "max", "min", "mean", "standardDeviation"
+        }));
+        ArrayList<String> stats2 = new ArrayList<>(List.of(new String[] {
+                "upperQuartile", "lowerQuartile", "skewness"
+        }));
+        assertCommandSuccess(new StatsCommand(stats1), model, expectedOutput1, model);
+        assertCommandSuccess(new StatsCommand(stats2), model, expectedOutput2, model);
     }
 }
