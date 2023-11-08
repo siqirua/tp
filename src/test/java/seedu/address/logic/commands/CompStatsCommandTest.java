d0b421a82cfd7bd6de9767e0c5713c1781e868b4
siqirua <lsq6918086@gmail.com>
2023-11-08 16:08:36 +0800

    Fix some checkstyle issues

--- src/test/java/seedu/address/logic/commands/CompStatsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CompStatsCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 import java.util.ArrayList;
@@ -17,25 +18,24 @@ import seedu.address.testutil.TestStudentDataUtil;
 import seedu.address.testutil.TestStudentScoreDataUtil;
 
 public class CompStatsCommandTest {
-    private Model model = new ModelManager(TestStudentDataUtil.getTestStudentBook("create"),
+    private final Model model = new ModelManager(TestStudentDataUtil.getTestStudentBook("create"),
             TestStudentScoreDataUtil.getSampleStudentScoreBook("twoScores"),
             TestGcDataUtil.getTestGcBook("create"), new UserPrefs());
-    private String expectedOutput = "Here are the statistics:\n"
-            + "MAX = 50.00, MIN = 0.00, MEAN = 25.00, STANDARD DEVIATION = 17.08\n"
-            + "UPPER QUARTILE = 42.50, LOWER QUARTILE = 7.50, SKEWNESS = 0.00\n";
-
-    private String expectedOutputOfTut3 = "Here are the statistics of Final of Tutorial Group T03:\n"
-            + "MAX = 100.00, MIN = 60.00, MEAN = 80.00, STANDARD DEVIATION = 14.14\n"
-            + "UPPER QUARTILE = 95.00, LOWER QUARTILE = 65.00, SKEWNESS = 0.00\n";
 
     @Test
     public void execute_stat_success() {
+        String expectedOutput = "Here are the statistics:\n"
+                + "MAX = 50.00, MIN = 0.00, MEAN = 25.00, STANDARD DEVIATION = 17.08\n"
+                + "UPPER QUARTILE = 42.50, LOWER QUARTILE = 7.50, SKEWNESS = 0.00\n";
         assertCommandSuccess(new CompStatsCommand(new ArrayList<>(), new GcName("Midterm")),
                 model, expectedOutput, model);
     }
 
     @Test
     public void execute_tutStat_success() {
+        String expectedOutputOfTut3 = "Here are the statistics of Final of Tutorial Group T03:\n"
+                + "MAX = 100.00, MIN = 60.00, MEAN = 80.00, STANDARD DEVIATION = 14.14\n"
+                + "UPPER QUARTILE = 95.00, LOWER QUARTILE = 65.00, SKEWNESS = 0.00\n";
         assertCommandSuccess(new CompStatsCommand(new ArrayList<>(), new GcName("Final"),
                         new TutorialGroup("T03")), model, expectedOutputOfTut3, model);
     }
