f67e1f38213426923a7d21de6cff14ebf1816ee0
siqirua <lsq6918086@gmail.com>
2023-11-11 12:16:57 +0800

    Implement test for SortStudentScoreCommandParser

---
+++ src/test/java/seedu/address/logic/parser/SortStudentScoreCommandParserTest.java
@@ -0,0 +1,20 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.SortStudentScoreCommand;
+import seedu.address.model.gradedcomponent.GcName;
+
+import static seedu.address.logic.parser.ParserTestUtil.assertParserSuccess;
+
+public class SortStudentScoreCommandParserTest {
+    private SortStudentScoreCommandParser parser = new SortStudentScoreCommandParser();
+
+    @Test
+    public void parse_sortStu_success() {
+        String inputWithComponent = " c/Midterm";
+        String inputWithBoth = " c/Final r/t";
+        assertParserSuccess(inputWithComponent, parser, new SortStudentScoreCommand(new GcName("Midterm"),
+                false));
+        assertParserSuccess(inputWithBoth, parser, new SortStudentScoreCommand(new GcName("Final"), true));
+    }
+}
